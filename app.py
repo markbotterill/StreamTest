6b32864bf8505668ce64f183bcb4e6a410915be5
Mark Botterill <markbotters@gmail.com>
2022-06-08 12:38:35 +0100

    added texT

--- app.py
+++ app.py
@@ -6,19 +6,4 @@ st.set_page_config(
     page_title="Speech-to-Text Transcription App", page_icon="👄", layout="wide"
 )
 
-def _max_width_():
-    max_width_str = f"max-width: 1200px;"
-    st.markdown(
-        f"""
-    <style>
-    .reportview-container .main .block-container{{
-        {max_width_str}
-    }}
-    </style>
-    """,
-        unsafe_allow_html=True,
-    )
-
-_max_width_()
-
-
+st.text("TEST")
